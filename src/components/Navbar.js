9312979efc6a28c1d72c332d257fc9c443179727
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-11-24 09:39:02 +0200

    started adding dark mode theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,13 @@ import {
   DribbbleIcon,
   GithubIcon,
   LinkedInIcon,
+  MoonIcon,
   PinterestIcon,
+  SunIcon,
   TwitterIcon,
 } from "./Icons";
 import { motion } from "framer-motion";
+import useThemeSwitcher from "./hooks/useThemeSwitcher";
 
 const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
@@ -18,7 +21,8 @@ const CustomLink = ({ href, title, className = "" }) => {
     <Link href={href} className={`${className} relative group`}>
       {title}
       <span
-        className={`h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+        className={`h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 
+        dark:bg-light ${
           router.asPath === href ? "w-full" : "w-0"
         }`}
       >
@@ -29,8 +33,11 @@ const CustomLink = ({ href, title, className = "" }) => {
 };
 
 const Navbar = () => {
+
+  const [mode, setMode] = useThemeSwitcher();
+
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
         <CustomLink href="/about" title="About" className="mx-4" />
@@ -83,6 +90,18 @@ const Navbar = () => {
         >
           <DribbbleIcon />
         </motion.a>
+
+        <button
+        onClick={() => setMode(mode === "light" ? "dark" : "light")}
+        className={`ml-3 flex items-center justify-center rounded-full p-1
+        ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark" }`}
+        >
+        {
+          mode === "dark" ?
+          <SunIcon className={"fill-dark"} />
+          : <MoonIcon className={"fill-dark"}/>
+        }
+        </button>
       </nav>
 
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
