febac8ecb544a7a9da5f14fbcc5c7f0654d5feef
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-04-22 15:36:16 -0600

    took off circles

--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -11,14 +11,14 @@ const LiIcon = ({reference}) => {
     )
   return (
     <figure className='absolute left-0 stroke-dark dark:stroke-light'>
-        <svg className="-rotate-90 md:w-[100px] md:h-[137px] xs:h-[148px] " width="100" height="75" viewbox='0 0 100 100'>
+        {/* <svg className="-rotate-90 " width="124" height="75" viewbox='0 0 100 100'>
             <circle cx="75" cy="50" r="20" className='stroke-primary dark:stroke-primaryDark stroke-1 fill-none'/>
             <motion.circle cx="75" cy="50" r="20" className='stroke-[5px] fill-light dark:fill-dark'
             style={{
                 pathLength : scrollYProgress
             }}/>
             <circle cx="75" cy="50" r="10" className='animate-pulse stroke-1 fill-primary dark:fill-primaryDark'/>
-        </svg>
+        </svg> */}
     </figure>
   )
 }
